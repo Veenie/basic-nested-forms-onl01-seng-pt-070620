d5a2a97664c95606be79500a65c215cea8d81421
Raveen K Sharma <raveen08@gmail.com>
2020-11-23 04:25:37 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,17 +16,8 @@ class PeopleController < ApplicationController
   end
 
   private
+  
+  
 
- params.require(:person).permit(
-    :name,
-    addresses_attributes: [
-      :street_address_1,
-      :street_address_2,
-      :city,
-      :state,
-      :zipcode,
-      :address_type
-    ]
-  )
-end
+
 end
